16886a5311927464efcb5354988e6e439109dde8
harshdeswal <harshdeswal03@yahoo.com>
2023-02-20 10:31:32 +0530

    email service done .Site complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,10 @@ import signature from '../media/signature.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 const Navbar = () => {
   const [navactive,setnavactive] = useState('false');
+  const router = useRouter();
   const handlenavbarclick = ()=>{
     if(navactive=='false'){
       setnavactive('true')
@@ -19,23 +21,23 @@ const Navbar = () => {
             <Image src={signature} className={styles.sign} height={60} width={120} />
 
         </div>
-        <div className={styles.nav_toggler} onClick={handlenavbarclick}>
+        <div className={navactive=='true'?`${styles.nav_toggler} ${styles.navactive}`:styles.nav_toggler} onClick={handlenavbarclick}>
             <span></span>
           
         </div>
         <div className={styles.right}>
-            <Link href='/'><button>Home </button></Link>
-            <Link href='/about'><button>About</button></Link>
-            <Link href='/projects'><button>Projects</button></Link>
-            <Link href='/contact'><button>Contact</button></Link>
+            <Link href='/'><button className={router.pathname=='/'?styles.pageactive:''}>Home </button></Link>
+            <Link href='/about'><button className={router.pathname=='/about'?styles.pageactive:''}>About</button></Link>
+            <Link href='/projects'><button className={router.pathname=='/projects'?styles.pageactive:''}>Projects</button></Link>
+            <Link href='/contact'><button className={router.pathname=='/contact'?styles.pageactive:''}>Contact</button></Link>
         </div>
         {
           navactive == 'true' ?
             <div className={styles.right1}>
-                <Link href='/'><button>Home </button></Link>
-                <Link href='/about'><button>About</button></Link>
-                <Link href='/projects'><button>Projects</button></Link>
-                <Link href='/contact'><button>Contact</button></Link>
+                <Link href='/'><button className={router.pathname=='/'?styles.pageactive:''}>Home </button></Link>
+                <Link href='/about'><button className={router.pathname=='/about'?styles.pageactive:''}>About</button></Link>
+                <Link href='/projects'><button className={router.pathname=='/projects'?styles.pageactive:''}>Projects</button></Link>
+                <Link href='/contact'><button className={router.pathname=='/contact'?styles.pageactive:''}>Contact</button></Link>
               </div>
               :
               <></>
--- pages/contact.js
+++ pages/contact.js
@@ -3,8 +3,20 @@ import styles from '../styles/Contact.module.css'
 import Image from 'next/legacy/image'
 import Navbar from '@/components/Navbar'
 import Homesection5 from '@/components/Homesection5'
+import emailjs from '@emailjs/browser'
 
 const contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_d3tibkl', 'template_dtwo6jj', e.target, 'c4764iGvPUUElUqPs')
+      .then((result) => {
+          alert('Message sent successfully');
+          e.target.reset()
+      }, (error) => {
+          alert(error.text);
+      });
+  }
   return (
     <div>
       <Navbar/>
@@ -15,11 +27,11 @@ const contact = () => {
             <div className={styles.img}>
               <Image src={contactpic} layout='responsive'/>
             </div>
-            <form>
+            <form onSubmit={sendEmail}>
               <p>Get in touch</p>
-              <input placeholder='Name'/>
-              <input placeholder='Email'/>
-              <textarea placeholder='Message'/>
+              <input placeholder='Name' name='user_name'/>
+              <input placeholder='Email' name='user_email' />
+              <textarea placeholder='Message' name='message' />
               <button>Send</button>
             </form>
           </div>
